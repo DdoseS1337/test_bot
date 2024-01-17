6eed9c63c8143764798d5deaa01e178652f267c6
unknown <yaroslav.maksymchuk@gmail.com>
2024-01-17 13:26:07 +0200

    test

--- src/Routes/user.ts
+++ src/Routes/user.ts
@@ -1,8 +1,8 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 import userController from "../users/user.controller";
 const router = Router();
 
 router.get("/", userController.create.bind(userController));
-router.post("/", userController.create.bind(userController));
+router.post("/", userController.get.bind(userController));
 
 export default router;
