624d7308611392108402230b803e6084092f853e
unknown <yaroslav.maksymchuk@gmail.com>
2024-01-17 13:26:21 +0200

    test

--- src/Routes/user.ts
+++ src/Routes/user.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import userController from "../users/user.controller";
 const router = Router();
 
-router.get("/", userController.create.bind(userController));
-router.post("/", userController.get.bind(userController));
+router.get("/", userController.get.bind(userController));
+router.post("/", userController.create.bind(userController));
 
 export default router;
