70adef5f81270a557b8112ad7bc4ec82b54c717b
DdoseS1337 <79210436+DdoseS1337@users.noreply.github.com>
2025-01-28 12:56:40 +0200

    Update user.repository.ts

--- src/users/user.repository.ts
+++ src/users/user.repository.ts
@@ -4,6 +4,7 @@ import { UserDocument } from "./interface/user.interface";
 
 export class UserRepository extends AbstractRepository<UserDocument> {
   constructor(model: Model<UserDocument>) {
+    
     super(model);
   }
 }
